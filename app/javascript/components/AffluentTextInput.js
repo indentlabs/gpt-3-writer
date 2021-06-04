e18878f28a29827580314d32e74ce4c99da25f5b
Andrew Brown <drusepth@gmail.com>
2021-06-04 11:41:30 -0700

    wip

--- app/javascript/components/AffluentTextInput.js
+++ app/javascript/components/AffluentTextInput.js
@@ -1,6 +1,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 
+import Typography from '@material-ui/core/Typography';
+
 class AffluentTextInput extends React.Component {
   constructor(props) {
     super(props);
@@ -12,14 +14,67 @@ class AffluentTextInput extends React.Component {
 
   render () {
     return (
-      <div contentEditable="true" 
-           id={this.props.id} 
-           className="card-panel"
-      >
-        <p>
+      <React.Fragment>
+        <Typography
+          paragraph
+          contentEditable="true" 
+          id={this.props.id} 
+          className="card-panel"
+        >
           {this.props.defaultText}
-        </p>
-      </div>
+        </Typography>
+        <div className="suggestions container-fluid">
+          <div className="row">
+            <div className="col l3">
+              <div className="grey-text center uppertext">Suggestion #1</div>
+              <div className="hoverable card suggestion-card">
+                <div className="card-content suggestion" contentEditable="true">
+                </div>
+                <div className="card-action center teal use-suggestion">
+                  <a href="#" className="white-text">Use this suggestion</a>
+                </div>
+              </div>
+            </div>
+            <div className="col l4">
+              <div className="grey-text center uppertext">Suggestion #2</div>
+              <div className="hoverable card suggestion-card">
+                <div className="card-content suggestion" contentEditable="true">
+                </div>
+                <div className="card-action center teal use-suggestion">
+                  <a href="#" className="white-text">Use this suggestion</a>
+                </div>
+              </div>
+            </div>
+            <div className="col l3">
+              <div className="grey-text center uppertext">Suggestion #3</div>
+              <div className="hoverable card suggestion-card">
+                <div className="card-content suggestion" contentEditable="true">
+                </div>
+                <div className="card-action center teal use-suggestion">
+                  <a href="#" className="white-text">Use this suggestion</a>
+                </div>
+              </div>
+            </div>
+            <div className="col l2">
+              <div className="grey-text center uppertext">Options</div>
+              <div className="center">
+                <a href="#" className="btn regenerate-suggestions">
+                  More suggestions
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div className="container">
+          <div className="helper-text grey-text">...and when you aren't sure how to continue, press this button!</div>
+          <div id="continue-writing" className="btn btn-large">GPT-3, take the wheel</div>
+        
+          <div className="progress" id="progress-bar">
+            <div className="indeterminate"></div>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
---
+++ app/javascript/components/Asimov.js
@@ -0,0 +1,167 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import { EventEmitter } from "../lib/event_emitter";
+
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+import AffluentTextInput from "./AffluentTextInput";
+
+const drawerWidth = 400;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: drawerWidth,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-start',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginRight: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: 0,
+  },
+}));
+
+export default function Asimov() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap className={classes.title}>
+            Asimov
+          </Typography>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="end"
+            onClick={handleDrawerOpen}
+            className={clsx(open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+        <AffluentTextInput id="editor" defaultText="Once upon a time there was a dragon named Dave that wanted to make friends, but" />
+      </main>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="right"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          My Awesome Story
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </div>
+  );
+};
