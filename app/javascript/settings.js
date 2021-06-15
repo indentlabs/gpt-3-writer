f057b272eeac8802a65b372e996f0a20d5d2b16a
Andrew Brown <drusepth@gmail.com>
2021-06-14 18:46:14 -0700

    two-way sync on models also

--- app/javascript/settings.js
+++ app/javascript/settings.js
@@ -1,6 +1,5 @@
 $(function() {
-
-
+  // Simplified architecture settings (sync to advanced)
   $('.simplified-assistant-option').on('click', function () {
     // Update UI to show this assistant is selected
     $('.simplified-assistant-option').removeClass('orange lighten-2');
@@ -8,5 +7,22 @@ $(function() {
 
     // Update actual data source fields
     $('#autoscribe-architecture').val($(this).data('architecture')).formSelect();
+  });
+
+  // Simplified model settings (sync to advanced)
+  $('input[name=simplified-model]').on('change', function() {
+    $('#autoscribe-model').val($(this).val()).formSelect();
+  });
+
+  // Advanced architecture change (sync to simplified)
+  $('#autoscribe-architecture').on('change', function () {
+    // Update UI to show this assistant is selected
+    $('.simplified-assistant-option').removeClass('orange lighten-2');
+    $('.simplified-assistant-option[data-architecture="' + $(this).val() + '"]').addClass('orange lighten-2');
+  });
+
+  // Advanced model change (sync to simplified)
+  $('#autoscribe-model').on('change', function () {
+    $('input[name=simplified-model][value="' + $(this).val() + '"]').prop('checked', true);
   });
 });
